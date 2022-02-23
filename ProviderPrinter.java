1000f7769fba73ee44b4d129233106c1995a3c4b
Plamena Petrova <99747710+Plamito012@users.noreply.github.com>
2022-02-23 14:41:29 +0200

    Add files via upload

---
+++ ProviderPrinter.java
@@ -0,0 +1,19 @@
+
+public class ProviderPrinter {
+int number;
+String name;
+int x, y;
+void println()
+void sum()
+System.out.println("Hello from printer!");
+System.exit (1);
+		
+  }
+	
+	
+	
+	
+
+}
+
+
